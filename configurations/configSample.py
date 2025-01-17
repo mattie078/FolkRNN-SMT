62c48d0ff054ca9cf4f42419eee09a6b9d7e8cf0
TychoPuente <tychopuente@hotmail.com>
2025-01-17 12:14:54 +0100

    add irishEurovision metadata

--- configurations/configSample.py
+++ configurations/configSample.py
@@ -9,9 +9,9 @@ learning_rate_decay_after = 20
 learning_rate_decay = 0.97
 
 batch_size = 32
-max_epoch = 4
+max_epoch = 200
 grad_clipping = 5
 validation_fraction = 0.05
 validate_every = 500 # iterations
 
-save_every = 1  # epochs
+save_every = 10  # epochs
